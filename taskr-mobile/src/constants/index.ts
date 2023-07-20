1097d7d34ad9dff79915e4395fd2c656d0fd9ffa
George <georgeey123@gmail.com>
2023-07-20 15:22:39 +0000

    changed the api url

--- taskr-mobile/src/constants/index.ts
+++ taskr-mobile/src/constants/index.ts
@@ -6,6 +6,6 @@ export const SCREEN_WIDTH = Dimensions.get("window").width;
 export const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 export const API_URL =
-  Constants.expoConfig.extra?.API_URL || "http://localhost:3030/api";
+  Constants.expoConfig.extra?.API_URL || "http://192.168.43.182:3030/api";
 
 export const TASKR_AUTH = "taskr-auth";
